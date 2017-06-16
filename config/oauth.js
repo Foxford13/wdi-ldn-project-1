63f9a8e3e0bbeff5b8d204fac1ad7c91a40525c3
Jakub Dziekan <jdziek89@gmail.com>
2017-06-16 09:53:21 +0100

    Updating oauth config

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     loginUrl: 'https://api.instagram.com/oauth/authorize/',
     accessTokenUrl: 'https://api.instagram.com/oauth/access_token',
-    redirectUri: 'http://localhost:8000/oauth/instagram',
+    redirectUri: process.env.INSTAGRAM_REDIRECT_URI || 'http://localhost:8000/oauth/instagram',
     clientId: process.env.INSTAGRAM_ID,
     clientSecret: process.env.INSTAGRAM_SECRET,
     responseCode: 'code',
